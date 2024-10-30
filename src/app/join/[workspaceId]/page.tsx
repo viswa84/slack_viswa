251b6438a7b270308548de2116f507b5cc95970f
viswa846 <154207669+viswa846@users.noreply.github.com>
2024-10-30 18:05:52 +0530

    verification input added

--- src/app/join/[workspaceId]/page.tsx
+++ src/app/join/[workspaceId]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-
+import VerificatonInput from "react-verification-input"
 //logo need_to_change
 const JoinPage = () => {
   return (
@@ -9,11 +9,17 @@ const JoinPage = () => {
       <Image src={"/logo.svg"} alt="Logo" width={60} height={60} />
       <div className="flex flex-col gap-y-4 items-center justify-center max-w-md">
         <div className="flex flex-col gap-y-2 items-center justify-center">
-            <h1>
-                Join workspace
-            </h1>
-
+          <h1>
+            Join workspace
+          </h1>
+          <p className="text-md text-muted-foreground">
+            Enter the workspace code to join
+          </p>
         </div>
+        <VerificatonInput classNames={{
+          container:"flex gap-x-2",
+          character:"uppercase h-auto rounded-md border border-gray-300 flex items-center text-lg font-medium"
+        }}/>
       </div>
     </div>
   );
